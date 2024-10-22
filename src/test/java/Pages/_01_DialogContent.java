edc2c5ba55d30fd1340f3b158a3f407ddf9cb778
Abylmyrat Gummanov <abdy.coding@gmail.com>
2024-10-22 13:10:05 +0300

    locators are done and written down into the dialogcontent class

--- src/test/java/Pages/_01_DialogContent.java
+++ src/test/java/Pages/_01_DialogContent.java
@@ -1,6 +1,8 @@
 package Pages;
 
 import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class _01_DialogContent extends ParentPage {
@@ -10,5 +12,36 @@ public class _01_DialogContent extends ParentPage {
     }
     // locatorlar bu satırdan alta yazılacak.
     // locators should be written down under this line
-}
+    @FindBy(id = "customer.firstName")
+    public WebElement firstName;
 
+    @FindBy(id = "customer.address.street")
+    public WebElement  address;
+    @FindBy(id = "customer.lastName")
+    public WebElement  lastName;
+    @FindBy(id = "customer.address.city")
+    public WebElement  city;
+    @FindBy(id = "customer.address.state")
+    public WebElement  state;
+    @FindBy(id = "customer.address.zipCode")
+    public WebElement  zipCode;
+    @FindBy(id = "customer.phoneNumber")
+    public WebElement  phoneNumber;
+    @FindBy(id = "customer.ssn")
+    public WebElement  ssN;
+
+    @FindBy(id = "customer.username")
+    public WebElement userName;
+
+    @FindBy(id = "customer.password")
+    public WebElement passWord;
+
+    @FindBy(id = "repeatedPassword")
+    public WebElement confirmPassword;
+
+    @FindBy(xpath = "//input[@value='Register']")
+    public WebElement register;
+
+    @FindBy(xpath = "//div[@id='rightPanel']/p[text()]")
+    public WebElement successMessage;
+}
